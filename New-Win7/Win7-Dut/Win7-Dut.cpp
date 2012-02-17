31c47c100603681b0c179b11b311dc496f1bff3a
Chandra Duba <cduba@wi-fi.org>
2012-02-17 01:44:15 +0000

    Adding New Win7 code - CPP converted to have 1) QoS traffic related fixes ( TDLS) 2) EAP and Security profiles related fixes ( hotSpot) 3) Traffic related fixes done by Qiumin ( TDLS)
    Updated version to 5.0.0 
    
    
    git-svn-id: file:///home/wifi/svnrepos/local-svn/SIG/trunk@118 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ New-Win7/Win7-Dut/Win7-Dut.cpp
@@ -0,0 +1,13 @@
+// Win7-Dut.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+extern int main(int , char **);
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return (main(argc,(char **)argv));
+	return 0;
+}
+
